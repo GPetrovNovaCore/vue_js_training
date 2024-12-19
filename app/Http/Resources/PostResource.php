1c6c069ccb977be4d7303a138ab157e08c126a4b
Georgi Petrov <gpetrov@novacore.bg>
2024-12-19 17:24:16 +0200

    Add category for posts + dummy data + pagination

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,10 +15,11 @@ class PostResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'content' => substr($this->content, 0, 50).'...',
-            'created_at' => $this->created_at->toDateString()
+            'id'            => $this->id,
+            'title'         => $this->title,
+            'content'       => substr($this->content, 0, 50).'...',
+            'category'      => $this->category->name,
+            'created_at'    => $this->created_at->toDateString()
         ];
     }
 }
--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -11,6 +11,9 @@
                             <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>
                         </th>
                         <th class="px-6 py-3 bg-gray-50 text-left">
+                            <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
                             <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
                         </th>
                         <th class="px-6 py-3 bg-gray-50 text-left">
@@ -19,12 +22,15 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                    <tr v-for="post in posts">
+                    <tr v-for="post in posts.data">
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                             {{ post.id }}
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                             {{ post.title }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                            {{ post.category }}
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                             {{ post.content }}
@@ -35,12 +41,15 @@
                     </tr>
                 </tbody>
             </table>
+
+            <TailwindPagination :data="posts" @pagination-change-page="getPosts" class="mt-4" />
         </div>
     </div>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
+import { TailwindPagination } from 'laravel-vue-pagination';
 import usePosts from "@/composables/posts";
 
 const { posts, getPosts } = usePosts();

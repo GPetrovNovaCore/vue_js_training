0369534defbf2de38b29ab3cb22914335c90f7be
Georgi Petrov <gpetrov@novacore.bg>
2025-01-11 01:01:37 +0200

    Login Cleanup and Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,12 @@ use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => 'api:sanctum'], function () {
+    Route::apiResource('posts', PostController::class);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
-Route::apiResource('posts', PostController::class);
-Route::get('categories', [CategoryController::class, 'index']);
+

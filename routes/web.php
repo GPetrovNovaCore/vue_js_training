af4bb04a08f82d9a0485ec3080d714773767c9cf
Georgi Petrov <gpetrov@novacore.bg>
2025-01-11 00:53:00 +0200

    Login form and first Authentication

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
+
 Route::view('/{any?}', 'dashboard')
     ->where('any', '.*');
